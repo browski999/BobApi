95303fc47012690661969b8c3dc90178b2be968c
Rob Wright <bobazure1969@gmail.com>
2023-08-04 06:46:39 +0100

    Updated weather types

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace BobApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Blazing"
+        "Freezing", "Blazing"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
